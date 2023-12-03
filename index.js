742b8cb6859fd5918eca8fe416ea803e4aa08833
Chandler Abril <abrilchandler1@gmail.com>
2023-12-02 20:01:42 -0700

    for my future

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const string = word.length;
+  for (let i = 0; i < string; i++) {
+   if (word[i] !== word[string - 1 -i]) {
+    return false
+   } 
+  }
+  return true;
 }
-
+isPalindrome("racecar")
+isPalindrome("abba")
+isPalindrome("robot")
+isPalindrome("a")
+isPalindrome("ab")
 /* 
   Add your pseudocode here
+  function isPalindrome(argument, "string")
+  if string is a palindrome, return true
+  if not, return false
 */
 
 /*
